34eaa344faaf1af03b954f82ee09cbac5a6abb5f
mafia555 <mafiya555@yandex.ru>
2023-12-24 23:31:27 +0500

    Pause Menu update!
    Added scripts for pause menu UI to work

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT THE GAME.");
+        Application.Quit();
+    }
+}
